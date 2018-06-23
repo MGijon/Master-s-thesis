f97647f7019715b0416ce20def9e55eb23f6960c
Unknown <mgijon94@gmail.com>
2018-06-23 17:03:11 +0200

    updated

--- Graphics_Random_SQL_1.py
+++ Graphics_Random_SQL_1.py
@@ -95,7 +95,7 @@ def car_and_Kanemaru():
 	plt.savefig('/Users/manuelgijon/Documents/Programación/Masters_thesis/Data/Images/Kanemau and car for a total of ' + str(cantidad1) + ' samples.png')
 	plt.show()
 
-car_and_Kanemaru()
+#car_and_Kanemaru()
 
 def Scatter_car_and_Kanemaru():
 	cursor.execute("SELECT Kanemaru_car_value FROM R1")
@@ -112,8 +112,90 @@ def Scatter_car_and_Kanemaru():
 	plt.savefig('/Users/manuelgijon/Documents/Programación/Masters_thesis/Data/Images/Scatter Kanemau and car for a total of ' + str(cantidad1) + ' samples.png')
 	plt.show()
 
-Scatter_car_and_Kanemaru()
+#Scatter_car_and_Kanemaru()
 
+def Rooftop_Comedy_car_value(rug = False):
+	cursor.execute("SELECT Rooftop_Comedy_car_value FROM R1")
+	datos = cursor.fetchall()
+	cantidad = len(datos)
+	plt.figure(figsize = (20, 7))
+	plt.title('+ Rooftop_Comedy - Car for a total of ' + str(cantidad) + ' samples.')
+	if rug:
+		sns.distplot(datos, rug = True)
+	else:
+		sns.distplot(datos)
+	plt.xlabel('Mean : ' + str(np.mean(datos)) + ' - STD : ' + str(np.std(datos)))
+	plt.savefig('/Users/manuelgijon/Documents/Programación/Masters_thesis/Data/Images/Rooftop_Comedy-car for a total of ' + str(cantidad) + ' samples.png')
+	plt.show()
+
+	plt.title('Cumulative: + Rooftop_Comedy - Car for a total of ' + str(cantidad) + ' samples.')
+	sns.distplot(datos,
+             hist_kws=dict(cumulative=True),
+             kde_kws=dict(cumulative=True))
+	plt.xlabel('Mean : ' + str(np.mean(datos)) + ' - STD : ' + str(np.std(datos)))
+	plt.savefig('/Users/manuelgijon/Documents/Programación/Masters_thesis/Data/Images/Rooftop_Comedy-car for a total of ' + str(cantidad) + ' samples_Cumulative.png')
+	plt.show()
+
+#Rooftop_Comedy_car_value()
+
+def car_Rooftop_Comedy_value(rug = False):
+	cursor.execute("SELECT car_Rooftop_Comedy_value FROM R1")
+	datos = cursor.fetchall()
+	cantidad = len(datos)
+	plt.figure(figsize = (20, 7))
+	plt.title('+ car - Rooftop_Comedy for a total of ' + str(cantidad) + ' samples.')
+	if rug:
+		sns.distplot(datos, rug = True)
+	else:
+		sns.distplot(datos)
+	plt.xlabel('Mean : ' + str(np.mean(datos)) + ' - STD : ' + str(np.std(datos)))
+	plt.savefig('/Users/manuelgijon/Documents/Programación/Masters_thesis/Data/Images/Car-Rooftop_Comedy for a total of ' + str(cantidad) + ' samples.png')
+	plt.show()
+
+	plt.title('Cumulative: + car - Rooftop_Comedy  for a total of ' + str(cantidad) + ' samples.')
+	sns.distplot(datos,
+             hist_kws=dict(cumulative=True),
+             kde_kws=dict(cumulative=True))
+	plt.xlabel('Mean : ' + str(np.mean(datos)) + ' - STD : ' + str(np.std(datos)))
+	plt.savefig('/Users/manuelgijon/Documents/Programación/Masters_thesis/Data/Images/Car-Rooftop_Comedy for a total of ' + str(cantidad) + ' samples_Cumulative.png')
+	plt.show()
+
+#car_Rooftop_Comedy_value()
+
+def car_RC():
+	cursor.execute("SELECT Rooftop_Comedy_car_value FROM R1")
+	datos1 = cursor.fetchall()
+	cantidad1 = len(datos1)
+	cursor.execute("SELECT car_Rooftop_Comedy_value FROM R1")
+	datos2 = cursor.fetchall()
+	cantidad2 = len(datos2)
+	plt.figure(figsize = (20, 7))
+	plt.title('Rooftop_Comedy and car for a total of ' + str(cantidad1) + ' samples.')
+	sns.distplot(datos1, label = '+ Rooftop_Comedy - car')
+	sns.distplot(datos2, label = '+ car - Rooftop_Comedy')
+	plt.legend()
+	plt.xlabel('+ Rooftop_Comedy - car -> Mean : ' + str(np.mean(datos1)) + ' - STD : ' + str(np.std(datos1)) + '\n+ car - Rooftop_Comedy -> Mean : ' + str(np.mean(datos2)) + ' - STD : ' + str(np.std(datos2)))
+	plt.savefig('/Users/manuelgijon/Documents/Programación/Masters_thesis/Data/Images/Rooftop_Comedy and car for a total of ' + str(cantidad1) + ' samples.png')
+	plt.show()
+
+#car_RC()
+
+def Scatter_car_and_RC():
+	cursor.execute("SELECT Rooftop_Comedy_car_value FROM R1")
+	datos1 = cursor.fetchall()
+	cantidad1 = len(datos1)
+	cursor.execute("SELECT car_Rooftop_Comedy_value FROM R1")
+	datos2 = cursor.fetchall()
+	cantidad2 = len(datos2)
+	plt.figure(figsize = (20, 7))
+	plt.title('Scatter Rooftop_Comedy and car for a total of ' + str(cantidad1) + ' samples.')
+	plt.scatter(datos1, datos2)
+	plt.ylabel('+ car - Rooftop_Comedy')
+	plt.xlabel('+ Rooftop_Comedy - car')
+	plt.savefig('/Users/manuelgijon/Documents/Programación/Masters_thesis/Data/Images/Scatter Rooftop_Comedy and car for a total of ' + str(cantidad1) + ' samples.png')
+	plt.show()
+
+Scatter_car_and_RC()
 
 # close the conexion with the data base
 conexion.close()
