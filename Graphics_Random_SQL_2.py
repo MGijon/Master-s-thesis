42490cfb9b67f20331546117c4bbbcaa0ea82fcc
Unknown <mgijon94@gmail.com>
2018-06-24 16:10:31 +0200

    updated

--- Graphics_Random_SQL_2.py
+++ Graphics_Random_SQL_2.py
@@ -117,9 +117,91 @@ def GRAPHIC_1(rug = False):
 
 #GRAPHIC_1()
 
+def GRAPHIC_2(rug = False):
+	'''
+	Naked_Juice_molecule_sequencing_VALUE
+	'''
+	cursor.execute("SELECT Naked_Juice_molecule_sequencing_VALUE FROM R2")
+	datos = cursor.fetchall()
+	cantidad = len(datos)
+	plt.figure(figsize = (20, 7))
+	plt.title('+ Naked_Juice - molecule_sequencing for a total of ' + str(cantidad) + ' samples.')
+	if rug:
+		sns.distplot(datos, rug = True)
+	else:
+		sns.distplot(datos)
+	plt.xlabel('Mean : ' + str(np.mean(datos)) + ' - STD : ' + str(np.std(datos)))
+	plt.savefig('/Users/manuelgijon/Documents/Programación/Masters_thesis/Data/Images/Naked_Juice-molecule_sequencing for a total of ' + str(cantidad) + ' samples.png')
+	plt.show()
 
+	plt.title('Cumulative: + Naked_Juice - molecule_sequencing for a total of ' + str(cantidad) + ' samples.')
+	sns.distplot(datos,
+             hist_kws=dict(cumulative=True),
+             kde_kws=dict(cumulative=True))
+	plt.xlabel('Mean : ' + str(np.mean(datos)) + ' - STD : ' + str(np.std(datos)))
+	plt.savefig('/Users/manuelgijon/Documents/Programación/Masters_thesis/Data/Images/Naked_Juice-molecule_sequencing for a total of ' + str(cantidad) + ' samples_Cumulative.png')
+	plt.show()
 
+	'''
+	molecule_sequencing_Naked_Juice_VALUE
+	'''
+	cursor.execute("SELECT molecule_sequencing_Naked_Juice_VALUE FROM R2")
+	datos = cursor.fetchall()
+	cantidad = len(datos)
+	plt.figure(figsize = (20, 7))
+	plt.title('+ molecule_sequencing - Naked_Juice for a total of ' + str(cantidad) + ' samples.')
+	if rug:
+		sns.distplot(datos, rug = True)
+	else:
+		sns.distplot(datos)
+	plt.xlabel('Mean : ' + str(np.mean(datos)) + ' - STD : ' + str(np.std(datos)))
+	plt.savefig('/Users/manuelgijon/Documents/Programación/Masters_thesis/Data/Images/molecule_sequencing-Naked_Juice for a total of ' + str(cantidad) + ' samples.png')
+	plt.show()
 
+	plt.title('Cumulative: + molecule_sequencing - Naked_Juice for a total of ' + str(cantidad) + ' samples.')
+	sns.distplot(datos,
+             hist_kws=dict(cumulative=True),
+             kde_kws=dict(cumulative=True))
+	plt.xlabel('Mean : ' + str(np.mean(datos)) + ' - STD : ' + str(np.std(datos)))
+	plt.savefig('/Users/manuelgijon/Documents/Programación/Masters_thesis/Data/Images/molecule_sequencing-Naked_Juice for a total of ' + str(cantidad) + ' samples_Cumulative.png')
+	plt.show()
+
+	'''
+	Naked_Juice_molecule_sequencing_VALUE & molecule_sequencing_Naked_Juice_VALUE
+	'''
+	cursor.execute("SELECT Naked_Juice_molecule_sequencing_VALUE FROM R2")
+	datos1 = cursor.fetchall()
+	cantidad1 = len(datos1)
+	cursor.execute("SELECT molecule_sequencing_Naked_Juice_VALUE FROM R2")
+	datos2 = cursor.fetchall()
+	cantidad2 = len(datos2)
+	plt.figure(figsize = (20, 7))
+	plt.title('Naked_Juice and molecule_sequencing for a total of ' + str(cantidad1) + ' samples.')
+	sns.distplot(datos1, label = '+ Naked_Juice - molecule_sequencing')
+	sns.distplot(datos2, label = '+ molecule_sequencing - Naked_Juice')
+	plt.legend()
+	plt.xlabel('+ Naked_Juice - molecule_sequencing -> Mean : ' + str(np.mean(datos1)) + ' - STD : ' + str(np.std(datos1)) + '\n+ molecule_sequencing - Naked_Juice -> Mean : ' + str(np.mean(datos2)) + ' - STD : ' + str(np.std(datos2)))
+	plt.savefig('/Users/manuelgijon/Documents/Programación/Masters_thesis/Data/Images/Naked_Juice and molecule_sequencing for a total of ' + str(cantidad1) + ' samples.png')
+	plt.show()
+
+	'''
+	Scatter Naked_Juice_molecule_sequencing_VALUE & molecule_sequencing_Naked_Juice_VALUE
+	'''
+	cursor.execute("SELECT Naked_Juice_molecule_sequencing_VALUE FROM R2")
+	datos1 = cursor.fetchall()
+	cantidad1 = len(datos1)
+	cursor.execute("SELECT molecule_sequencing_Naked_Juice_VALUE FROM R2")
+	datos2 = cursor.fetchall()
+	cantidad2 = len(datos2)
+	plt.figure(figsize = (20, 7))
+	plt.title('Scatter Naked_Juice and molecule_sequencing for a total of ' + str(cantidad1) + ' samples.')
+	plt.scatter(datos1, datos2)
+	plt.ylabel('+ Naked_Juice - molecule_sequencing')
+	plt.xlabel('+ molecule_sequencing - Naked_Juice')
+	plt.savefig('/Users/manuelgijon/Documents/Programación/Masters_thesis/Data/Images/Scatter Naked_Juice and molecule_sequencing for a total of ' + str(cantidad1) + ' samples.png')
+	plt.show()
+
+#GRAPHIC_2()
 
 
 # close the conexion with the data base
